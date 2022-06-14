8517f4d5a5ea767c0d9edf0671dfeae42f821c2e
Gregoire Legrand <gregoire.legrand56@gmail.com>
2022-06-14 09:42:10 +0200

    commit qui rajoute les classes vehicules et conducteurs

---
+++ src/main/java/fr/univartois/butinfo/ihm/GestionVentes/model/conducteur/Livreur.java
@@ -0,0 +1,15 @@
+package conducteur;
+
+import com.modeliosoft.modelio.javadesigner.annotations.objid;
+
+@objid ("75e74a9d-2605-4446-97ab-910f99d021c5")
+public class Livreur {
+    @objid ("42179da5-22c8-48e9-9a90-27d4a38975a1")
+    public Livreur(String nom, String prenom, String numeroPermis, String typePermis) {
+    }
+
+    @objid ("70d1e121-1f7d-44c8-865e-b6dc15126917")
+    public void nbHConduite() {
+    }
+
+}
